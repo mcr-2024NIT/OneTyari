48d49a438ce87d4272de91673955592ef8e4a81f
川鍋春輝 <125I039@stu.nit.ac.jp>
2026-04-27 14:18:20 +0900

    Initial commit

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float _speed;
+    public float _jumpForce;
+    public float _Xpos;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        _Xpos = transform.position.x;
+        Debug.Log("X座標、" + _Xpos);
+        transform.Translate(_speed * Time.deltaTime, 0f, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(0f, _jumpForce, 0f, ForceMode.Impulse);
+            Debug.Log("ジャンプ成功");
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        
+    }
+}
